54403d1721f681e52d6aa451fce548e3954d1bd7
chex <tuw111@hotmail.com>
2017-09-28 09:24:36 +0800

    修改问题

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ var escaper = require("true-html-escape");
 // var dotenv = require('dotenv');
 // dotenv.config();
 mongoose.connect('mongodb://localhost:27017/test');
-    var db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', function() {
-      console.log("数据库连接上了")
-    });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("数据库连接上了")
+});
 var Crawler = require("crawler");
 var Article = require('./module/Article');
 var User = require('./module/User');
 var c = new Crawler({
-    maxConnections: 20,
+    maxConnections: 50,
     // This will be called for each crawled page
     callback: function(error, res, done) {
         if (error) {
@@ -56,15 +56,21 @@ var c = new Crawler({
 });
 
 // Queue just one URL, with default callback
-function initPa(){
+function initPa() {
+    //var arr=[];
     for (var i = 1; i <= 10; i++) {
-        c.queue({ uri: 'http://www.jianshu.com/c/e50258a6a44b?order_by=added_at&page=' + i });
+        // arr.push('http://www.jianshu.com/c/e50258a6a44b?order_by=added_at&page=' + i)
+        setTimeout(function() {
+            c.queue('http://www.jianshu.com/c/e50258a6a44b?order_by=added_at&page=' + i)
+        })
+
     }
+
 }
 
-setInterval(function(){
+setInterval(function() {
     initPa();
-},60*60*1000)
+}, 60 * 60 * 1000)
 
 initPa();
 
@@ -81,9 +87,13 @@ function paArticle(url, userUrl, obj) {
                 var $ = res.$;
                 var content = $('.author').next();
                 content.find('.image-caption').each(function(i) {
-                    if ($(this).text == '图片发自简书App') {
-                        $(this).text = '图片来源未知';
+                    if ($(this).text() == '图片发自简书App') {
+                        $(this).text('图片来自异次元');
                     }
+                })
+                content.find('img').each(function() {
+                    var src = $(this).attr('src');
+                    $(this).attr('src', 'http:' + src);
                 })
                 contentHtml = escaper.unescape(content.html());
                 var article = Object.assign({}, { content: contentHtml }, obj);
@@ -112,25 +122,23 @@ function paUser(url, article) {
                     avatar: avatar,
                     name: name
                 }
-
                 saveData(article, user);
             }
             done();
         }
     }]);
 }
+var i = 0;
 
 function saveData(article, userObj) {
     var user = new User(userObj);
-    //user.notes.push(article1)
     user.save(function(err) {
-        if (err) return;
         var articleObj = Object.assign({}, {
             _creator: user._id
         }, article)
         var article1 = new Article(articleObj);
         article1.save(function(err) {
-            if (err) return;
+            if (err) { return };
         })
     })
 
